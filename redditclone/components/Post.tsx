cea0f58353dfabc57f77fd2006304b4a575ae377
Kashish Singh <54403943+singh-kashish@users.noreply.github.com>
2022-07-06 20:47:25 +0530

    Installed react-time-ago, added styles to globals.css, added thing to Post.tsx

--- redditclone/components/Post.tsx
+++ redditclone/components/Post.tsx
@@ -8,6 +8,7 @@ import {
   GiftIcon,
   ShareIcon,
 } from "@heroicons/react/outline";
+import Avatar from "./Avatar";
 
 type Props = {
   post: Post;
@@ -15,17 +16,23 @@ type Props = {
 
 const Post = ({ post }: Props) => {
   return (
-    <div>
+    <div className="flex cursor-pointer rounded-md border border-gray-300 bg-white shadow-sm hover:border-gray-600">
       {/* Votes ->lhs */}
-      <div>
-        <ArrowUpIcon className="voteButtons hover:text-red-400"/>
-        <p>0</p>
-        <ArrowDownIcon className="voteButtons hover:text-blue-400"/>
+      <div className="flex flex-col items-center justify-start space-y-1 rounded-l-md bg-gray-50 p-4 text-gray-400">
+        <ArrowUpIcon className="voteButtons hover:text-red-400" />
+        <p className="text-xs font-bold text-black">0</p>
+        <ArrowDownIcon className="voteButtons hover:text-blue-400" />
       </div>
       {/* body of post->rhs */}
-      <div>
+      <div className="p-3 pb-1">
         {/* Header */}
-
+        <div>
+          <Avatar seed={post?.subreddit[0]?.topic} />
+          <p>
+            <span>{post.subreddit[0]?.topic}</span> Posted by u/
+            {post.username ? post.username : "Guest"}{" "}
+          </p>
+        </div>
         {/* Body */}
         {/* Image */}
         {/* Footer */}
